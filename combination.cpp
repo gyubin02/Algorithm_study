a0326bf6c5e7a41a28f94c1dd90b1d0a4e2deb0e
gyubin02 <dlrbqls980@gmail.com>
2023-12-29 16:51:27 +0900

    메모이제이션으로 이항계수 구하기

--- combination.cpp
+++ combination.cpp
@@ -6,13 +6,13 @@ int comb(int n, int r);
 int cache[30][30];
 
 int main() {
-	for (int i = 0; i < 30; i++)
-		for (int j = 0; j < 30; j++)
-			cache[i][j] = -1;
+	memset(cache, -1, sizeof(cache));
+	cout << comb(4, 2);
 }
 
 int comb(int n, int r) { //n >= r이라고 가정합니다.
 	if (r == 0 || r == n) return 1;
-	if (cache[n][r] != -1) return cache[n][r];
-	return cache[n][r] = comb(n - 1, r - 1) + comb(n - 1, r);
+	int& ret = cache[n][r];
+	if (ret != -1) return ret;
+	return ret = comb(n - 1, r - 1) + comb(n - 1, r);
 }
