933d26f0febd07ea709fce687f65f5cbf6924482
gyubin02 <dlrbqls980@gmail.com>
2024-01-10 13:44:02 +0900

    Union-Find 구현

--- union-find.cpp
+++ union-find.cpp
@@ -6,9 +6,10 @@ int parent[MAX_N];
 int node[MAX_N];
 
 int find(int here) {
-	if (parent[here] == here) 
-		return here;
-	return parent[here] = find(parent[here]);
+	int& ret = parent[here];
+	if (ret == here)
+		return ret;
+	return ret = find(ret);
 }
 
 bool merge(int a, int b) { //cyle이 발생하면 false
