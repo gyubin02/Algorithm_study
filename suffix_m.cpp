2d6e415bf4415229800489433a96d0483e746747
gyubin02 <dlrbqls980@gmail.com>
2024-01-26 19:27:01 +0900

    맨버-마이어스 알고리

---
+++ suffix_m.cpp
@@ -0,0 +1,49 @@
+//맨버-마이어스 알고리즘
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+struct suffixComparator {
+	int t;
+	vector<int>& group;
+	suffixComparator(int t, vector<int>& group) :t(t),group(group) {}
+	bool operator()(int i, int j) {
+		if (group[i] != group[j])
+			return group[i] < group[j];
+		return group[i + t] < group[j + t];
+	}
+};
+vector<int> getSuffixArray(const string& s);
+
+int main() {
+	string s;
+	cin >> s;
+	vector<int> ret = getSuffixArray(s);
+	for (int i = 0; i < ret.size(); i++)
+		cout << ret[i] << "\n";
+}
+
+vector<int> getSuffixArray(const string& s) {
+	int N = s.size();
+	vector<int> ret(N), group(N + 1);
+	group[N] = -1;
+	for (int i = 0; i < N; i++)
+		ret[i] = i;
+	for (int i = 0; i < N; i++)
+		group[i] = s[i];
+	for (int t = 1; 2 * t <= N; t *= 2) {
+		suffixComparator comp = suffixComparator(t, group);
+		sort(ret.begin(), ret.end(), comp);
+		vector<int> newGroup(N + 1);
+		newGroup[ret[0]] = 0;
+		newGroup[N] = -1;
+		for (int j = 1; j < N; j++) 
+			if (comp(ret[j - 1], ret[j]))
+				newGroup[ret[j]] = newGroup[ret[j - 1]] + 1;
+			else
+				newGroup[ret[j]] = newGroup[ret[j - 1]];
+		group = newGroup;
+	}
+	return ret;
+}
