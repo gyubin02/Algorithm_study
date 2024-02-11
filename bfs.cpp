d83777cd1fd70e7008d64449d6c4ab1fe73e6d57
gyubin02 <dlrbqls980@gmail.com>
2024-02-11 17:37:29 +0900

    너비 우선 탐색

--- bfs.cpp
+++ bfs.cpp
@@ -6,22 +6,36 @@ using namespace std;
 
 const int N;
 vector<vector<int>> adj; //인접 리스트
-vector<bool> discovered;
+vector<int> distancee, parent;
 void bfs(int s) {
-	discovered = vector<bool>(N, false);
+	distancee = vector<int>(N, -1);
+	parent = vector<int>(N, -1);
 	queue<int> q;
 	q.push(s);
-	discovered[s] = true;
+	distancee[s] = 0;
+	parent[s] = s;
 	while (!q.empty()) {
 		int here = q.front();
 		q.pop();
 		cout << "visit : " << here << "\n";
 		for (int i = 0; i < adj[here].size(); i++) {
 			int there = adj[here][i];
-			if (!discovered[there]) {
+			if (distancee[there] == -1) {
 				q.push(there);
-				discovered[there] = true;
+				distancee[there] = distancee[here] + 1;
+				parent[there] = here;
 			}
 		}
 	}
+}
+
+vector<int> shortestPath(int v, vector<int>& parent) { //bfs 스패닝 트리 parent와 정점 v가 주어질 때, v부터 루트까지의 경로를 반환
+	vector<int> path(1, v);
+	int p = path[0];
+	while (true) {
+		p = parent[p];
+		path.push_back(p);
+		if (parent[p] == p) break;
+	}
+	return path;
 }
